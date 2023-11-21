6019d9ef093fca32ec2230a424076d43f5e85fe7
Mike Provence <49548172+M-PROV@users.noreply.github.com>
2023-11-21 13:45:43 -0800

    Got the javascript working properly. Will add HTML and CSS later.

--- script.js
+++ script.js
@@ -8,14 +8,9 @@ let playerScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
-    let number = Math.floor(Math.random() * 1000);
-    if (number % 3 === 0) {
-        return 'rock';
-    } else if (number % 3 === 1) {
-        return 'paper';
-    } else {
-        return 'scissor';
-    }
+    let choices = ['rock', 'paper', 'scissor'];
+    let rps = choices[(Math.floor(Math.random() * choices.length))];
+    return rps;
 }
 
 function getPlayerChoice() {
@@ -23,46 +18,50 @@ function getPlayerChoice() {
     return pick.toLowerCase();
 }
 
-function playGame(playerSelection, computerSelection) {
-    getComputerChoice();
-    getPlayerChoice();
-    computerSelection = getComputerChoice();
-    playerSelection = getPlayerChoice();
+    
 
-    if (
-        (playerSelection === 'rock' && computerSelection === 'scissor') ||
-        (playerSelection === 'paper' && computerSelection === 'rock') ||
-        (playerSelection === 'scissor' && computerSelection === 'paper' && 
-            computerScore <= 5 || playerScore <= 5 )
-    ) {
-        playerScore++;
-        if (playerScore >= 5) {
-            console.log('Player pick: ' + playerSelection + ' Computer pick: ' + computerSelection + ' You got to 5 first! You win! Player: ' + playerScore + ' Computer: ' + computerScore);
-        } else {
-        console.log('Player pick: ' + playerSelection + ' Computer pick: ' + computerSelection + ' You win! Score is now Player: ' + playerScore + ' Computer: ' + computerScore);
-        getComputerChoice();
-        getPlayerChoice();
-        }
+function playGame() {
+
+    let computerSelection = getComputerChoice();
+    let playerSelection = getPlayerChoice();
+
+    while (playerScore < 5 && computerScore < 5) {
+
         
-    } else if (
-        (playerSelection === 'rock' && computerSelection === 'paper') ||
-        (playerSelection === 'paper' && computerSelection === 'scissor') ||
-        (playerSelection === 'scissor' && computerSelection === 'rock')
-    ) {
-        computerScore++;
-        if (computerScore >= 5) {
-            console.log('Player pick: ' + playerSelection + ' Computer pick: ' + computerSelection + ' Computer got to 5 first. You lose! Player: ' + playerScore + ' Computer: ' + computerScore);
+
+    if (playerSelection === 'rock' && computerSelection === 'scissor' ||
+        playerSelection === 'paper' && computerSelection === 'rock' ||
+        playerSelection === 'scissor' && computerSelection === 'paper') {
+            playerScore++;
+            if ( playerScore >= 5 ) {
+                console.log(playerSelection + ' beats ' + computerSelection + '. You win! Current score is Player: ' + playerScore + ' Computer: ' + computerScore + '. You have beaten the computer. Congratulations!');
+            } else {
+            console.log(playerSelection + ' beats ' + computerSelection + '. You win! Current score is Player: ' + playerScore + ' Computer: ' + computerScore );
+            computerSelection = getComputerChoice();
+            getPlayerChoice();
+            }
+        } else if (
+            playerSelection === 'rock' && computerSelection === 'paper' ||
+            playerSelection === 'paper' && computerSelection === 'scissor' ||
+            playerSelection === 'scissor' && computerSelection === 'rock'
+        ) {
+            computerScore++;
+            if ( computerScore >= 5) {
+                console.log( computerSelection + ' beats ' + playerSelection + '. You lose! Current score is Player: ' + playerScore + ' Computer: ' + computerScore + '. You have lost to the computer. Game Over');
+            } else {
+                console.log( computerSelection + ' beats ' + playerSelection + '. You lose! Current score is Player: ' + playerScore + ' Computer: ' + computerScore);
+                computerSelection = getComputerChoice();
+                getPlayerChoice();
+            }
         } else {
-            console.log('Player pick: ' + playerSelection + ' Computer pick: ' + computerSelection + ' You lose! Player: ' + playerScore + ' Computer: ' + computerScore);
-            getComputerChoice();
+            console.log(playerSelection + ' ties ' + computerSelection + '. It is a tie! Current score is Player: ' + playerScore + ' Computer: ' + computerScore);
+            computerSelection = getComputerChoice();
             getPlayerChoice();
         }
-    } else {
-        console.log('Player pick: ' + playerSelection + ' Computer pick: ' + computerSelection + ' The round is a tie! Player: ' + playerScore + ' Computer: ' + computerScore);
-        getComputerChoice();
-        getPlayerChoice();
     }
 }
+
+
 
 playGame();
 
